6fea141458701d4e11b056ab3b2ed7d473fc8f72
TrinhQuangvo <voquang1406@gmail.com>
2018-06-13 13:11:31 +0700

    Bài 15 , chỉnh sửa AddProduct View and Controller

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Http\Requests\AddProductRequest;
+use App\Models\Product;
+//lấy danh mục 
+use App\Models\Category;
 class ProductController extends Controller
 {
 
@@ -15,12 +18,31 @@ class ProductController extends Controller
 
     public function getAddProduct()
     {
-        return view('backend.addproduct');
+        $data['catelist'] = Category::all();
+        return view('backend.addproduct',$data  );
     }
 
-    public function postAddProduct()
+    public function postAddProduct(AddProductRequest $request)
     {
+        // lấy tên ảnh 
+        $filename= $request->img->getClientOriginalName();
         // tạo request đê validate form 
+        $product = new Product;
+        $product->prod_name	 = $request->name;
+        $product->prod_slug  = str_slug($request->name);
+        $product->img = $filename;
+        $product->accessories = $request->accessories;
+        $product->price = format_number($request->price);
+        $product->warranty = $request->warranty;
+        $product->promotion = $request->promotion;
+        $product->condition = $request->condition; 
+        $product->status = $request->status;
+        $product->description = $request->description;
+        $product->featured = $request->featured;
+        $product->save();
+
+        // upload ảnh 
+        $request ->img ->storeAs('avatar',$filename);
     }
 
     public function getEditProduct()
--- resources/views/backend/addproduct.blade.php
+++ resources/views/backend/addproduct.blade.php
@@ -11,14 +11,20 @@
         <form class="form-group" action="/action_page.php">
             <div class="form-inline">
                 <label for="">Tên Sản Phẩm :</label>
-                <label style="margin-left:90px"for="pwd">Giá:</label><br>
+                <label style="margin-left:95px"for="price">Giá:</label>
+                <label  style="margin-left:175px" for="cate">Danh Mục</label><br>
                 <input type="text" class="form-control" name="name">
                 <input type="text" class="form-control" name="price">
+                <select  name="cate" class="form-control" id="">
+                    @foreach($catelist as $cate)
+                    <option value="{{$cate->id}}">{{$cate->cate_name}}</option>
+                    @endforeach
+                </select>
             </div>
             <div class="form-inline" style="margin-top:30px">
                 <label for="">Hình Ảnh</label><br>
                 <input type='file' class="form-control" id="inputFile" />
-                <img  id="image_upload_preview" height="90" width="160" alt="your image" />
+                <img  id="image_upload_preview" name="thumbnails" height="90" width="160" alt="your image" />
                 <script>
                     function readURL(input) {
                         if (input.files && input.files[0]) {
@@ -39,25 +45,47 @@
             
             <div class="form-group" style="margin-top:80px">
                 <label for="">Mô Tả</label>
-                <textarea name="editor1"></textarea>
+                <textarea name="description"></textarea>
                 <script>
-                    CKEDITOR.replace( 'editor1' );
+                    CKEDITOR.replace( 'description' );
                 </script>
             </div>
             
             
             <div class="form-inline">
                 <label for="">Tình Trạng</label>
-                <select class="form-control" id="sel1">
+                <label style="margin-left:90px" for="">Bảo Hành</label>
+                <label style="margin-left:175px" for="">Phụ Kiện</label>
+                <br>
+                <select class="form-control" name="condition" id="sel1">
                     <option>Mới</option>
                     <option>Like New</option>
                 </select>
-
-                <label for=""></label>
+                <input style="margin-left:55px" class="form-control" type="text" value="12 Tháng Trên Toàn Quốc" name="warranty" id="">
+                <select style="margin-left:35px" class="form-control" name="accessories">
+                    <option>Cable , Sạc ,Tai Nghe</option>
+                    <option>Cable , Sạc</option>
+                </select>
             </div>
-            <div class="form-group">
+            <div style="margin-top:30px" class="form-inline">
+                <label for="">Khuyến Mãi</label>
+                <label style="margin-left:155px" for="">Status</label>
+                <br>
+                <input type="text" class="form-control" name="promotion" value="Miếng Dán Cường Lực" id="">
+                <select style="margin-left:35px" class="form-control" name="status">
+                    <option value="1">Chưa Có Hàng</option>
+                    <option value="2">Mới về</option>
+                    <option value="3">Tạm Hết Hàng</option>
+                    <option value="4">Ngừng Kinh Doanh</option>
+                </select>
             </div>
-            <button type="submit" class="btn btn-primary">Tạo Mới</button>
+            <div style="margin-top:20px" class="form-group">
+                <label for="">Sản Phẩm Nổi Bậc</label><br>
+                Có <input type="radio" name="featured" id="">
+                Không <input type="radio" checked name="featured" value="0" id="">
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary">Tạo Mới</button>
+            {{csrf_field()}}
         </form>
       <br><br>
     </div>
