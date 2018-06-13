1fb2a7b618dba4dbdab9a462ce9066d287b3ba6a
TrinhQuangvo <voquang1406@gmail.com>
2018-06-13 10:24:50 +0700

    @@ comit again wtf just happened ??

--- resources/views/backend/addproduct.blade.php
+++ resources/views/backend/addproduct.blade.php
@@ -1,7 +1,7 @@
 @extends('backend.master')
 @section('title','Thêm Sản Phẩm')
 @section('main')
-<div class="col-sm-9">
+<div class="col-sm-7">
       <h4><small>Thêm Sản Phẩm</small></h4>
       <hr>
       <h2>Thêm Sản Phẩm</h2>
@@ -13,12 +13,12 @@
                 <label for="">Tên Sản Phẩm :</label>
                 <label style="margin-left:90px"for="pwd">Giá:</label><br>
                 <input type="text" class="form-control" name="name">
-                <input type="text" class="form-control" id="pwd">
+                <input type="text" class="form-control" name="price">
             </div>
             <div class="form-inline" style="margin-top:30px">
                 <label for="">Hình Ảnh</label><br>
                 <input type='file' class="form-control" id="inputFile" />
-                <img  id="image_upload_preview" height="160" width="120" alt="your image" />
+                <img  id="image_upload_preview" height="90" width="160" alt="your image" />
                 <script>
                     function readURL(input) {
                         if (input.files && input.files[0]) {
@@ -36,11 +36,15 @@
                     });
                 </script>
             </div>
-            <div class="form-group" style="margin-top:30px">
+            
+            <div class="form-group" style="margin-top:80px">
                 <label for="">Mô Tả</label>
-                <textarea name="" class="form-control" rows="4" required="required"></textarea>
-                `
+                <textarea name="editor1"></textarea>
+                <script>
+                    CKEDITOR.replace( 'editor1' );
+                </script>
             </div>
+            
             
             <div class="form-inline">
                 <label for="">Tình Trạng</label>
@@ -48,10 +52,12 @@
                     <option>Mới</option>
                     <option>Like New</option>
                 </select>
+
+                <label for=""></label>
             </div>
             <div class="form-group">
             </div>
-            <button type="submit" class="btn btn-default">Submit</button>
+            <button type="submit" class="btn btn-primary">Tạo Mới</button>
         </form>
       <br><br>
     </div>
